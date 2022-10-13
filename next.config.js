7060cfa05a30b8007b72f2ffbca106119f63e609
Yeolha Sung <tjddufgk@gmail.com>
2022-10-14 04:53:37 +0900

    add product list mock data

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: [
+      'newsimg-hams.hankookilbo.com',
+      'img.danawa.com',
+      'mblogthumb-phinf.pstatic.net',
+      'encrypted-tbn0.gstatic.com',
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ src/data/productList.ts
@@ -0,0 +1,28 @@
+import { Product } from '../types/product';
+
+export const productList: Product[] = [
+  {
+    id: 0,
+    image:
+      'https://newsimg-hams.hankookilbo.com/2022/04/03/4f348e8b-5a91-471f-a986-879c2e04c755.jpg',
+    name: '바실리 체어',
+  },
+  {
+    id: 1,
+    image:
+      'https://img.danawa.com/prod_img/500000/978/699/img/9699978_1.jpg?shrink=330:330&_v=20220623104535',
+    name: '허먼밀러 에어론',
+  },
+  {
+    id: 2,
+    image:
+      'https://mblogthumb-phinf.pstatic.net/MjAxODAzMTVfMTE1/MDAxNTIxMTEwODczOTAw.C-icl-eauFetntqVvUiuIEHnqI8ovKTBYAbF24tVtBcg.fLFYNNhrfEIGX1HARYHFacZuXX3X8v_vNDSrmDHBaOMg.JPEG.vlsrj/image_139348701521110782782.jpg?type=w800',
+    name: '프리츠 한센',
+  },
+  {
+    id: 3,
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1DB2LPAzFEohHPec-vhwVtHWrs3-SXISYIQ&usqp=CAU',
+    name: 'USM HALLER',
+  },
+];
